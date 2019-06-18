55fc4c61d739af87e0494a068147b44a60711e81
stevelinf <51755603+stevelinf@users.noreply.github.com>
2019-06-18 22:00:32 +0800

    Add files via upload

---
+++ 題目12. 遞迴程式練習.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+    if(n == 0 || n == 1)
+        return (n + 1);
+    else
+        return (f(n - 1) + f(floor(n / 2)));
+}
+
+int main(void){
+    int k;
+    int result;
+    while(cin >> k){
+        result = f(k);
+        cout << result << endl;
+    }
+
+    return 0;
+}
